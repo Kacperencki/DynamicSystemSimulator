c94e6a2f130ba48fb39c515d6b50fd9c8d88c032
kapir <kapirozycki@gmail.com>
2025-07-26 12:06:26 +0200

    Add default values for t_span and t_eval

--- simulator.py
+++ simulator.py
@@ -6,11 +6,19 @@ from pendulum import Pendulum
 
 
 class Simulator:
-    def __init__(self, system, t_span, theta0, t_eval):
+    def __init__(self, system, theta0, t_span=None, t_eval=None):
         self.system = system
-        self.t_span = t_span
         self.theta0 = theta0
-        self.t_eval = t_eval
+
+        if t_span is None:
+            self.t_span = [0, 10]
+        else:
+            self.t_span = t_span
+
+        if t_eval is None:
+            self.t_eval = np.linspace(0, 10, 500)
+        else:
+            self.t_eval = t_eval
 
     def run(self):
         return scipy.integrate.solve_ivp(self.system.dynamics, self.t_span, self.theta0, t_eval=self.t_eval)
