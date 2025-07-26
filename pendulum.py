d80865c3fe6264423927f3f1ec58ab41862c147d
kapir <kapirozycki@gmail.com>
2025-07-26 12:47:23 +0200

    Fix pednulum equation

--- pendulum.py
+++ pendulum.py
@@ -17,6 +17,6 @@ class Pendulum:
     def dynamics(self, t, y):
         u, v = y # u = angle, v = angular velocity
         du_dt = v
-        dv_dt = self.d*v/(self.m*self.l*self.l) -self.g / self.l * np.sin(u)
+        dv_dt = - self.d*v/(self.m*self.l*self.l) -self.g / self.l * np.sin(u)
         return [du_dt, dv_dt]
 
