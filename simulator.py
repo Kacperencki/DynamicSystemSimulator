ad52c1ee0501a3f2bcf6bdfacf4f108a181d03b7
kapir <kapirozycki@gmail.com>
2025-07-27 18:08:51 +0200

    Changed Simulator class to know executing all other classes inside it for cleaner usage

--- simulator.py
+++ simulator.py
@@ -1,31 +1,19 @@
-import numpy as np
-import scipy
+
 from pendulum import Pendulum
-
-
+from visualizer import MatplotlibVisualizer
+from solver import Solver
+import numpy as np
+import matplotlib as plt
 
 
 class Simulator:
-    def __init__(self, system, theta0=None, t_span=None, t_eval=None):
+    def __init__(self, system):
         self.system = system
-        self.theta0 = theta0
 
-        if theta0 is None:
-            self.theta0 = [self.system.ang_pos, self.system.ang_vel]
-        else:
-            self.theta0 = theta0
+    def simulate(self):
+        result = Solver(self.system).run()
+        vis = MatplotlibVisualizer(self.system)
+        vis.animation(result.y[0]) # y[0] is the theta array needed for calculating point change for pendulum
 
-        if t_span is None:
-            self.t_span = [0, 10]
-        else:
-            self.t_span = t_span
-
-        if t_eval is None:
-            self.t_eval = np.linspace(0, 10, 500)
-        else:
-            self.t_eval = t_eval
-
-    def run(self):
-        return scipy.integrate.solve_ivp(self.system.dynamics, self.t_span, self.theta0, t_eval=self.t_eval)
 
 
