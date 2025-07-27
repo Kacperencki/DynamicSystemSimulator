9f1e0a88cd300c3999d6bcf5d6ec4b5a260338e0
kapir <kapirozycki@gmail.com>
2025-07-27 18:07:49 +0200

    add animation func to Viusalizer class (previously used in Simulator)

--- visualizer.py
+++ visualizer.py
@@ -2,20 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class MatplotlibVisualizer:
-    def __init__(self, length):
-        self.l = length
+    def __init__(self, system):
+        self.system = system
 
-    def draw_frame(self, theta):
-        x = self.l * np.sin(theta)
-        y = -self.l * np.cos(theta)
-
+    def pendulum_frame(self, theta): # DO przerobienia
+        x, y = self.system.point_coordinates(theta=theta)
+        l = self.system.l
         plt.clf()  # clear figure for the next frame
         plt.plot([0, x], [0, y], 'k-', linewidth=2)  # pendulum rod
         plt.plot(x, y, 'ro', markersize=10)  # pendulum bob
-        plt.xlim(-self.l - 0.5, self.l + 0.5)
-        plt.ylim(-self.l - 0.5, 0.5)
+        plt.xlim(-l - 0.5, l + 0.5)
+        plt.ylim(-l - 0.5, 0.5)
         plt.gca().set_aspect('equal')
         plt.pause(0.01)
 
-    def show(self):
+    def animation(self, theta_array):
+        for theta in theta_array:
+            self.pendulum_frame(theta)
+
         plt.show()
