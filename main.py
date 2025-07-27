8e0c5c1bc8f7b7045fb8ecb49d419d19aad0ebe2
kapir <kapirozycki@gmail.com>
2025-07-27 18:09:51 +0200

    changed main file using Simulator class

--- main.py
+++ main.py
@@ -2,20 +2,14 @@
 from simulator import Simulator
 from pendulum import Pendulum
 from visualizer import MatplotlibVisualizer
-
+from solver import Solver
 import numpy as np
 import matplotlib as plt
 
 def main():
-    pendulum = Pendulum(length=2, mass=2, damping=1, ang_position=np.radians(45),ang_velocity=0)
-    #theta0 = [np.radians(45), 0]
-    sim = Simulator(pendulum)
-    result = sim.run()
-
-    vis = MatplotlibVisualizer(pendulum.l)
-    for theta in result.y[0]:
-        vis.draw_frame(theta)
-    vis.show()
+    pendulum = Pendulum(length=2, mass=2, damping=1, ang_position=np.pi/4,ang_velocity=0)
+    simul = Simulator(pendulum)
+    simul.simulate()
 
 if __name__ == "__main__":
     main()
