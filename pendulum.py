ed112a90e45b251c046e9153b4f3a627707b4187
kapir <kapirozycki@gmail.com>
2025-09-23 20:57:56 +0200

    Added simple driven pendulum

--- pendulum.py
+++ pendulum.py
@@ -3,12 +3,17 @@ import numpy as np
 
 
 class Pendulum:
-    def __init__(self, length, mass, mode, damping=0.0, gravity=9.81, mass_model="point", I=None, lc=None):
-        self.l = length
-        self.m = mass
-        self.b = damping
-        self.g = gravity
+    def __init__(self, length, mass, mode, damping=0.0, drive_amplitude=2, drive_frequency=2.0, drive_phase=0.0, gravity=9.81, mass_model="point", I=None, lc=None):
+        self.l = length # [m]
+        self.m = mass # [kg]
+        self.b = damping # [N*m*s/rad]
+        self.g = gravity # [m/s^2]
+
         self.mass_model = mass_model
+
+        self.a = drive_amplitude # [N*m]
+        self.f = drive_frequency # [rad/s]
+        self.p = drive_phase # [rad]
 
 
         #Sprawdzanie jaki moment bezwładności wybrać
@@ -21,7 +26,7 @@ class Pendulum:
         else:
             raise ValueError(f"Unknown mass_model: {mass_model}")
 
-        self.I = moment_of_inertia if I is None else I   # I to bezwładność (moment of inertia)
+        self.I = moment_of_inertia if I is None else I   # I to bezwładność (moment of inertia) [kg*m^2]
         self.lc = mass_center_distance if lc is None else lc  # lc to odległość do środka masy (length to center)
 
 
@@ -29,7 +34,8 @@ class Pendulum:
         self.mode = mode
         self.mode_map = {
             "damped": self.dynamics_damped,
-            "ideal": self.dynamics_ideal
+            "ideal": self.dynamics_ideal,
+            "driven": self.dynamics_driven
         }
         if self.mode not in self.mode_map:
             raise ValueError(f"Unknown mode {self.mode}")
@@ -57,6 +63,17 @@ class Pendulum:
 
         return np.array([theta_dt, dtheta_dt])
 
+    def dynamics_driven(self, t, state):
+        theta, theta_dot = state
+
+        tau_drive = self.a * np.cos(self.f * t + self.p)
+        theta_dt = theta_dot
+        dtheta_dt = (tau_drive-self.b * theta_dot - self.m *self.g * self.lc * np.sin(theta))/self.I
+
+        return np.array([theta_dt, dtheta_dt])
+
+
+
 
     def positions(self, state):
         theta, theta_dot = state
@@ -69,7 +86,6 @@ class Pendulum:
         Possible change that to include more options for visualizer to take like type of blob to draw on which point
         """
         return [(0, 0),(x_center, y_center), (x_tip, y_tip)] # Wymaga poprawy w visualizerze
-
 
     #Funkcja sprawdza energie kinetyczną i potencjalną
     def energy_check(self, state):
