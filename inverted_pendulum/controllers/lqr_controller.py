e86da76168ff837164cedf0415dbe884007e9620
kapir <kapirozycki@gmail.com>
2025-10-04 18:21:51 +0200

    change lqr controller varaible and function names, also added generla workflow note about what this class does

--- inverted_pendulum/controllers/lqr_controller.py
+++ inverted_pendulum/controllers/lqr_controller.py
@@ -1,35 +1,58 @@
 from scipy.linalg import solve_continuous_are
 import numpy as np
-from DynamicSystemSimulator.inverted_pendulum.linearize import AB_analytic
+from DynamicSystemSimulator.inverted_pendulum.linearize import ideal_AB
 
 def wrap_to_pi(theta):
     return ((theta + np.pi) % (2*np.pi)) - np.pi # bounds the theta degree [-np.pi, np.pi] so for example 3pi = 1pi
 
+
+"""
+    Linear-Quadratic Regulator (LQR) for cart-pole near the upright.
+
+    Workflow:
+      1) Build (A,B) for the plant linearized at upright (theta=0).
+      2) Solve CARE(A,B,Q,R) -> P, then K = R^{-1} B^T P.
+      3) Given state s=[x,xdot,theta,thetadot], compute a wrapped small-angle
+         deviation and apply u = -K * (s - s_ref), then clip to actuator limits.
+
+    Notes:
+      - The angle used in feedback is 'wrapped' so the linear controller always
+        sees a small angle error around 0 (or theta_ref), not e.g. +2π.
+      - If the plant parameters change (M,m,l,g), call `compute_gain()` again.
+      - Q and R are specified in units of the *state deviations* and input.
+    """
+
 class LQRController:
-    def __init__(self, system, Q=None, R=None, force_limit=30, use_numeric=False):
+    def __init__(self, system, Q=None, R=None, force_limit=30, use_numeric=False,
+                 x_ref=0.0, theta_reff=0.0):
+
         self.system = system
+
         self.Q = np.diag([1.0, 0.1, 200.0, 5.0]) if Q is None else Q # state weight matrix [x, x_dot, theta, theta_dot] importance
         self.R = np.array([[0.5]]) if R is None else R # control weight matrix
+
+        self.x_ref = x_ref
+        self.theta_reff = theta_reff
+
         self.F_max = force_limit
         self.K = None
         self.compute_gain()
+
     def compute_gain(self):
-        A, B = AB_analytic(self.system.M, self.system.m, self.system.l, self.system.g)
+        A, B = ideal_AB(self.system.M, self.system.m, self.system.l, self.system.g)
 
         P = solve_continuous_are(A, B, self.Q, self.R) #Solves the continuous-time algebraic Riccati equation
+        # @ is the operator for multiplying matixes
         self.K = np.linalg.inv(self.R) @ B.T @ P # Matrix multiplication K is [1x4]
 
 
-
-
-
-    def __call__(self, t, state):
+    def cart_force(self, t, state):
 
         x, x_dot, theta, theta_dot = state
 
-        theta_error = wrap_to_pi(theta=theta)
+        theta_wrapped = wrap_to_pi(theta=(theta- self.theta_reff))
 
-        current_state_array = np.array([x, x_dot, theta_error, theta_dot])
+        current_state_array = np.array([x - self.x_ref, x_dot, theta_wrapped, theta_dot])
 
         u = float(-self.K @ current_state_array) # u is the Cart force
 
