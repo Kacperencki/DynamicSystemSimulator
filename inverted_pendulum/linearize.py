da487340a0292df4e24999d4e866e3863d3c8277
kapir <kapirozycki@gmail.com>
2025-10-04 17:28:28 +0200

    Added linearization of A, B matrices for damped dynamics

--- inverted_pendulum/linearize.py
+++ inverted_pendulum/linearize.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def AB_analytic(M, m, l, g=9.81):
+def ideal_AB(M, m, l, g=9.81):
     A = np.array([[0, 1, 0, 0],
                   [0, 0, -(m * g) / M, 0],
                   [0, 0, 0, 1],
@@ -8,3 +8,11 @@ def AB_analytic(M, m, l, g=9.81):
     B = np.array([[0], [1 / M], [0], [-1 / (M * l)]], dtype=float)
     return A, B
 
+
+def damped_AB(M, m, l, g=9.81, b_cart=0.0, b_pend=0.0):
+    A, B = ideal_AB(M, m, l, g)
+
+    A[1,1] = -b_cart / M # somethin wrong here
+    A[3,1] = -b_pend / (m * l**2)
+
+    return A, B
